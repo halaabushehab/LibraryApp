6b5de7c4b2ee9ffa704fd399ae1bf44b3fda9998
halaabushehab <hala.abushiihab@gmail.com>
2025-01-27 11:19:37 +0300

    react

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # LibraryApp
+
+# React
+#### is an open-source JavaScript library for building user interfaces. Developed by Facebook, it is used to create interactive web applications. React relies on reusable components and efficiently manages state, making it easier to develop dynamic applications.4
+In this task, we created a Library Management App using React. We started by setting up a new repository and React app using the npx create-react-app command. We then defined an initial state containing book data and created components like Header, Main, and Footer. In the Main component, we displayed the list of books using a card layout to show details like the title, author, and ISBN number.
---
+++ src/Main.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+         <div className="container my-5">
+        <h2 className="text-center mb-4">Books Available</h2>
+        <div className="row">
+          {this.props.books.map((book) => (
+            <div key={book.id} className="col-md-4 mb-4">
+              <div className="card h-100 shadow-sm">
+                <div className="card-body">
+                  <h5 className="card-title">{book.title}</h5>
+                  <p className="card-text">
+                    <strong>Author:</strong> {book.author}
+                  </p>
+                  <p className="card-text">
+                    <strong>ISBN:</strong> {book.isbn}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      </main>
+    );
+  }
+}
+
+export default Main;
